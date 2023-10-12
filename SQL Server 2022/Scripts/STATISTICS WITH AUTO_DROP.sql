a0cd36fcc3c68192fc57204bb55d94700940445d
Thorsten Kansy <tkansy@dotnetconsulting.eu>
2023-10-12 12:09:45 +0200

    Auto commit

---
+++ SQL Server 2022/Scripts/STATISTICS WITH AUTO_DROP.sql
@@ -0,0 +1,13 @@
+-- Disclaimer:
+-- Dieser Quellcode ist als Vorlage oder als Ideengeber gedacht. Er kann frei und ohne 
+-- Auflagen oder Einschränkungen verwendet oder verändert werden.
+-- Es wird keine Garantie übernommen, dass eine Funktionsfähigkeit mit aktuellen und 
+-- zukünftigen API-Versionen besteht. Der Autor übernimmt daher keine direkte oder indirekte 
+-- Verantwortung, wenn dieser Code gar nicht oder nur fehlerhaft ausgeführt wird.
+-- Für Anregungen und Fragen steht der Autor gerne zur Verfügung.
+
+-- Thorsten Kansy, www.dotnetconsulting.eu
+
+UPDATE STATISTICS ... WITH AUTO_DROP = ON;
+
+CREATE STATISTICS ... WITH AUTO_DROP = ON;
---
+++ SQL Server 2022/Scripts/XML Compression.sql
@@ -0,0 +1,11 @@
+-- Disclaimer:
+-- Dieser Quellcode ist als Vorlage oder als Ideengeber gedacht. Er kann frei und ohne 
+-- Auflagen oder Einschränkungen verwendet oder verändert werden.
+-- Es wird keine Garantie übernommen, dass eine Funktionsfähigkeit mit aktuellen und 
+-- zukünftigen API-Versionen besteht. Der Autor übernimmt daher keine direkte oder indirekte 
+-- Verantwortung, wenn dieser Code gar nicht oder nur fehlerhaft ausgeführt wird.
+-- Für Anregungen und Fragen steht der Autor gerne zur Verfügung.
+
+-- Thorsten Kansy, www.dotnetconsulting.eu
+
+ALTER INDEX PK_Table ON [dbo].[Table] REBUILD WITJH (XML_COMPRESSION = ON);
